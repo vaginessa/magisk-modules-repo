041c1724f5c25a1719e6594fd82559b90accd8a5
ya0211 <2337380138@qq.com>
2022-08-30 21:36:17 +0800

    timestamp: 2022-08-30 21:36:17.649644

--- util/sync.py
+++ util/sync.py
@@ -4,53 +4,51 @@ import subprocess
 from datetime import datetime
 from pathlib import Path
 
-from modules import write_file, load_file, load_url, MDict, MList
-from downloader import download_by_requests
-from read_module import read_module_prop
+from object import dict_, list_
+from file import load_json, load_json_url, write_json, get_props, download_by_requests
 
 
 class Sync:
     def __init__(self):
         self.root_folder = Path(__file__).resolve().parent
         self.repo_folder = self.root_folder.parent.joinpath("repo")
-        self.modules_json_file = self.repo_folder.joinpath("modules.json")
+        self._file = self.repo_folder.joinpath("modules.json")
 
-        self.modules_json_data: MDict = load_file(self.modules_json_file)
-        self.modules_list: MList = self.modules_json_data.modules
+        self._dict = load_json(self._file).dict_
+        self._list = list_(self._dict.modules).dict2dict_
 
     def write_modules_json(self):
-        self.modules_json_file = self.repo_folder.joinpath("modules.json")
-        modules_json_bak = self.repo_folder.joinpath("modules.json.bak")
+        file_bak = self.repo_folder.joinpath("modules.json.bak")
 
-        print("\nmoving old modules.json to modules.json.bak")
-        shutil.move(self.modules_json_file, modules_json_bak)
+        print("\nfile: moving old modules.json to modules.json.bak")
+        shutil.move(self._file, file_bak)
 
-        print("writing new modules.json")
-        write_file(self.modules_json_data.dict, self.modules_json_file)
+        print("file: writing new modules.json")
+        write_json(self._dict.dict, self._file)
 
     @staticmethod
     def download_file(url, out: Path):
-        print("downloading {0}".format(out.name))
+        print("file: download {0}".format(out.name))
         download_by_requests(url, out)
 
     @staticmethod
     def move_file(file_dir: Path, version):
         file_name = "{0}-{1}.zip".format(file_dir.name.split('.')[0], version)
-        print("move {0} to {1}".format(file_dir.name, file_name))
+        print("file: move {0} to {1}".format(file_dir.name, file_name))
         shutil.move(file_dir, file_dir.parent.joinpath(file_name))
 
     @staticmethod
-    def update_info(item: MDict, file_dir: Path):
-        prop = read_module_prop(file_dir)
-        item["name"] = prop.name
-        item["version"] = prop.version
-        item["versionCode"] = prop.versionCode
-        item["author"] = prop.author
-        item["description"] = prop.description
+    def update_info(item: dict_, file_dir: Path):
+        prop = get_props(file_dir).dict_
+        item.name = prop.name
+        item.version = prop.version
+        item.versionCode = prop.versionCode
+        item.author = prop.author
+        item.description = prop.description
 
     def pull(self, download_all=True):
-        for item in self.modules_list:
-            print("\nmodule id: {0}".format(item.id))
+        for item in self._list:
+            print("\nid: {0}".format(item.id))
 
             module_dir = self.repo_folder.joinpath(item.id)
             if not module_dir.exists():
@@ -59,7 +57,7 @@ class Sync:
             file_dir = module_dir.joinpath("{0}.zip".format(item.id))
 
             if item.updateJson != "":
-                update_json = load_url(item.updateJson)
+                update_json = load_json_url(item.updateJson).dict_
 
                 if "versionCode" in item:
                     if int(update_json.versionCode) > int(item.versionCode):
@@ -67,7 +65,7 @@ class Sync:
                         self.update_info(item, file_dir)
                         self.move_file(file_dir, item.version)
 
-                        item["status"] = {
+                        item.status = {
                             "zipUrl": update_json.zipUrl,
                             "changelog": update_json.changelog
                         }
@@ -87,12 +85,12 @@ class Sync:
             else:
                 print("{0} a no updateJson".format(item.id))
                 if download_all:
-                    self.download_file(item.status.zipUrl, file_dir)
+                    self.download_file(item.status.get("zipUrl"), file_dir)
                     self.update_info(item, file_dir)
                     self.move_file(file_dir, item.version)
 
-        self.modules_json_data["timestamp"] = str(datetime.now())
-        self.modules_json_data["modules"] = self.modules_list.mdict2dict()
+        self._dict.timestamp = str(datetime.now())
+        self._dict.modules = self._list.dict_2dict
         self.write_modules_json()
 
     def push(self):

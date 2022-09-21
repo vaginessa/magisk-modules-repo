900b3192f4bc398f3e54d2745cc1f463e7a5263c
ya0211 <2337380138@qq.com>
2022-09-21 18:56:42 +0800

    timestamp: 2022-09-21 18:56:42.392578

--- util/sync.py
+++ util/sync.py
@@ -1,12 +1,18 @@
 import os
 import shutil
 import subprocess
-from datetime import datetime
+from glob import glob
 from pathlib import Path
+from datetime import datetime
 
 from object import dict_, list_
 from file import load_json, load_json_url, write_json, get_props, download_by_requests
 from progress import Progress
+
+GITHUB_RAW_URL = "https://raw.githubusercontent.com"
+MODULE_REPO = "ya0211/magisk-modules-repo"
+REPO_BRANCH = "main"
+REPO_URL = "{0}/{1}/{2}".format(GITHUB_RAW_URL, MODULE_REPO, REPO_BRANCH)
 
 
 def have_update_json(item, file):
@@ -16,20 +22,20 @@ def have_update_json(item, file):
         if int(update_json.versionCode) > int(item.versionCode):
             download_by_requests(update_json.zipUrl, file)
             update_info(item, file)
-            shutil.move(file, file.parent.joinpath("{0}-{1}.zip".format(file.name.split('.')[0], item.version)))
+            shutil.move(file, file.parent.joinpath("{0}.zip".format(item.version.replace(" ", "_"))))
 
             item.status = {
-                "zipUrl": update_json.zipUrl,
+                "zipUrl": "{0}/{1}/{2}.zip".format(REPO_URL, item.id, item.version.replace(" ", "_")),
                 "changelog": update_json.changelog
             }
 
     elif not file.exists():
         download_by_requests(update_json.zipUrl, file)
         update_info(item, file)
-        shutil.move(file, file.parent.joinpath("{0}-{1}.zip".format(file.name.split('.')[0], item.version)))
+        shutil.move(file, file.parent.joinpath("{0}.zip".format(item.version.replace(" ", "_"))))
 
         item["status"] = {
-            "zipUrl": update_json.zipUrl,
+            "zipUrl": "{0}/{1}/{2}.zip".format(REPO_URL, item.id, item.version.replace(" ", "_")),
             "changelog": update_json.changelog
         }
 
@@ -43,7 +49,7 @@ def update_info(item: dict_, file: Path):
     item.description = prop.description
 
 
-def pull(json_dict: dict_, modules_folder: Path, json_file: Path, download_all=True):
+def pull(json_dict: dict_, modules_folder: Path, json_file: Path, update_all=True):
     json_list = list_(json_dict.modules).dict2dict_
 
     pro = Progress(json_list.size, bar_length=60)
@@ -52,6 +58,9 @@ def pull(json_dict: dict_, modules_folder: Path, json_file: Path, download_all=T
         item_dir = modules_folder.joinpath(item.id)
         if not item_dir.exists():
             os.makedirs(item_dir)
+        else:
+            for file in sorted(glob("{0}/*".format(item_dir))):
+                os.remove(file)
 
         file = item_dir.joinpath("{0}.zip".format(item.id))
 
@@ -59,10 +68,10 @@ def pull(json_dict: dict_, modules_folder: Path, json_file: Path, download_all=T
             have_update_json(item, file)
 
         else:
-            if download_all:
+            if update_all:
                 download_by_requests(item.status.get("zipUrl"), file)
                 update_info(item, file)
-                shutil.move(file, file.parent.joinpath("{0}-{1}.zip".format(file.name.split('.')[0], item.version)))
+                os.remove(file)
 
         pro.progress_default()
 
